a0c49b806ba15dcb7a05dce7f59903501d4f820d
SamDC <sam.davis.castro@gmail.com>
2019-01-21 00:29:19 -0500

    combined userStatuseducer and messageReducer

--- src/message-board.js
+++ src/message-board.js
@@ -1,6 +1,4 @@
-import {
-    createStore
-} from 'redux';
+import {createStore, combineReducers} from 'redux';
 
 export const ONLINE = `ONLINE`;
 export const AWAY = `AWAY`;
@@ -8,50 +6,85 @@ export const BUSY = `BUSY`;
 export const OFFLINE = `OFFLINE`;
 export const UPDATE_STATUS = `UPDATE_STATUS`;
 
+export const CREATE_NEW_MESSAGE = `CREATE_NEW_MESSAGE`;
+
 const defaultState = {
-    messages: [
-        {
-            date: new Date('2019-1-20 10:00:00'),
-            postedBy: 'Stan',
-            content: `Omg they killed kenny!`
-        },  {
-            date: new Date('2019-1-20 10:01:00'),
-            postedBy: 'Kyle',
-            content: `You bastards!`
-        },  {
-            date: new Date('2019-1-20 10:02:00'),
-            postedBy: 'Kenny',
-            content: `No, I'm ok...`
-        },  {
-            date: new Date('2019-1-20 10:04:00'),
-            postedBy: 'Cartman',
-            content: `Screw you guys, I'm going home.`
-        }
-    ],
+    messages: [{
+        date: new Date('2019-1-20 10:00:00'),
+        postedBy: 'Stan',
+        content: `Omg they killed kenny!`
+    }, {
+        date: new Date('2019-1-20 10:01:00'),
+        postedBy: 'Kyle',
+        content: `You bastards!`
+    }, {
+        date: new Date('2019-1-20 10:02:00'),
+        postedBy: 'Kenny',
+        content: `No, I'm ok...`
+    }, {
+        date: new Date('2019-1-20 10:04:00'),
+        postedBy: 'Cartman',
+        content: `Screw you guys, I'm going home.`
+    }],
     userStatus: `ONLINE`
 }
 
-const reducer = (state = defaultState, {type, value})=> {
-    console.log(state);
-    console.log(type, value);
-    switch(type) {
-        case `UPDATE_STATUS`:
-            return {...state, userStatus:value};
+
+const userStatusReducer = (state = defaultState.userStatus, {type, value}) => {
+    switch (type) {
+        case UPDATE_STATUS:
+            return value;
             break;
     }
     return state;
 }
+const messageReducer = (state = defaultState.messages, {type, value, postedBy, date}) => {
+    switch (type) {
+        case CREATE_NEW_MESSAGE:
+            const newState = [{date, postedBy, content:value},...state];//arrays = mutable, make copy instead of altering original
+            return newState;
+    }
+}
+const combinedReducer = combineReducers({
+    userStatus: userStatusReducer,
+    messages: messageReducer
+});
 
-const store = createStore(reducer);
+const store = createStore(combinedReducer);
 
 const render = () => {
-    const { messages, userStatus } = store.getState();
+    const {
+        messages,
+        userStatus
+    } = store.getState();
     document.getElementById('messages').innerHTML = messages
-    .sort((a,b) => b.date - a.date)
-    .map(message => (`
+        .sort((a, b) => b.date - a.date)
+        .map(message => (`
         <div>${message.postedBy} : ${message.content}</div>
         `)).join('');
-        document.form.newMessage.fields.disabled = (userStatus === OFFLINE);
+    document.forms.newMessage.fields.disabled = (userStatus === OFFLINE);
 };
 
+const statusUpdateAction = (value) => {
+    return {
+        type: UPDATE_STATUS,
+        value
+    }
+}
+const newMessageAction = (content, postedBy) => {
+    const date = new Date();
+    return {
+        type: CREATE_NEW_MESSAGE,
+        value: content,
+        postedBy,
+        date
+    }
+}
+
+document.forms.selectStatus.addEventListener('change', (e) => {
+    store.dispatch(statusUpdateAction(e.target.value));
+});
+
 render();
+
+store.subscribe(render);
