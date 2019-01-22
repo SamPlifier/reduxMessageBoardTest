491351e6d19b6a3292d688eefc10e4c088a38a49
SamDC <sam.davis.castro@gmail.com>
2019-01-21 19:52:13 -0500

    style and functionality updates to tasks

--- src/tasks.js
+++ src/tasks.js
@@ -1,6 +1,10 @@
-import { Dispatcher, ReduceStore } from './flux';
-import { generate as id } from 'shortid';
-
+import {
+    Dispatcher,
+    ReduceStore
+} from './flux';
+import {
+    generate as id
+} from 'shortid';
 
 const tasksDispatcher = new Dispatcher();
 
@@ -8,14 +12,14 @@ const CREATE_TASK = `CREATE_TASK`;
 const COMPLETE_TASK = `COMPLETE_TASK`;
 const SHOW_TASKS = `SHOW_TASKS`;
 
-const createNewTaskAction = (content)=>{
+const createNewTaskAction = (content) => {
     return {
         type: CREATE_TASK,
         value: content
     }
 };
 
-const completeTaskAction = (id,isComplete)=>{
+const completeTaskAction = (id, isComplete) => {
     return {
         type: COMPLETE_TASK,
         id,
@@ -23,7 +27,7 @@ const completeTaskAction = (id,isComplete)=>{
     }
 };
 
-const showTasksAction = (show)=>{
+const showTasksAction = (show) => {
     return {
         type: SHOW_TASKS,
         value: show
@@ -33,52 +37,111 @@ const showTasksAction = (show)=>{
 const initialState = {
     tasks: [{
         id: id(),
-        content: "Update CSS styles",
-        complete: false
-    }, {
-        id: id(),
-        content: "Add unit tests",
-        complete: false
-    }, {
-        id: id(),
-        content: "Post to social media",
-        complete: false
-    },{
-        id: id(),
-        content: "Install hard drive",
+        content: `Adobe Experience Manager Authoring Fundamentals`,
         complete: true
+    }, {
+        id: id(),
+        content: `Google Analytics Fundamentals`,
+        complete: true
+    }, {
+        id: id(),
+        content: `Node.js: Getting Started`,
+        complete: true
+    }, {
+        id: id(),
+        content: `Advanced Node.js`,
+        complete: true
+    }, {
+        id: id(),
+        content: `React.js: Getting Started`,
+        complete: true
+    }, {
+        id: id(),
+        content: `Building Apps with React and Flux`,
+        complete: true
+    }, {
+        id: id(),
+        content: `Mastering Flux and Redux`,
+        complete: true
+    }, {
+        id: id(),
+        content: `Angular: Getting Started`,
+        complete: true
+    }, {
+        id: id(),
+        content: `HTML5 Fundamentals`,
+        complete: true,
+        link: ``
+    }, {
+        id: id(),
+        content: `D3.js Data Visualization Fundamentals`,
+        complete: false,
+        link: `https://app.pluralsight.com/library/courses/d3js-data-visualization-fundamentals/table-of-contents`
+    }, {
+        id: id(),
+        content: `Continue Rapid ES6 Training`,
+        complete: false,
+        link: `https://app.pluralsight.com/library/courses/rapid-es6-training/table-of-contents`
+    }, {
+        id: id(),
+        content: `Building Apps with React & Redux in ES6`,
+        complete: false,
+        link: `https://app.pluralsight.com/library/courses/react-redux-react-router-es6/table-of-contents`
+    }, {
+        id: id(),
+        content: `Typescript Fundamentals`,
+        complete: false,
+        link: `https://app.pluralsight.com/library/courses/typescript/table-of-contents`
+    }, {
+        id: id(),
+        content: `Angular Fundamentals`,
+        complete: false,
+        link:`https://app.pluralsight.com/library/courses/angular-fundamentals/table-of-contents`
+    }, {
+        id: id(),
+        content: `Building Web Apps with Node.js and Express 4.0`,
+        complete: false,
+        link: `https://app.pluralsight.com/library/courses/nodejs-express-web-applications-update/table-of-contents`
     }],
-    showComplete:true
+    showComplete: true
 };
 
 class TasksStore extends ReduceStore {
     getInitialState() {
         return initialState;
     }
-    reduce(state,action){
+    reduce(state, action) {
         let newState;
-        console.log("Processing action",action.type);
-        switch(action.type) {
+        console.log("Processing action", action.type);
+        switch (action.type) {
             case CREATE_TASK:
-                newState = { ...state, tasks: [ ... state.tasks ]};
+                newState = { ...state,
+                    tasks: [...state.tasks]
+                };
                 newState.tasks.push({
-                    id:id(),
-                    content:action.value,
+                    id: id(),
+                    content: action.value,
                     complete: false
                 });
                 return newState;
             case COMPLETE_TASK:
-                newState = { ... state, tasks: [ ... state.tasks ]};
-                const affectedElementIndex = newState.tasks.findIndex(t=>t.id === action.id);
-                newState.tasks[affectedElementIndex] = { ... state.tasks[affectedElementIndex], complete: action.value }
+                newState = { ...state,
+                    tasks: [...state.tasks]
+                };
+                const affectedElementIndex = newState.tasks.findIndex(t => t.id === action.id);
+                newState.tasks[affectedElementIndex] = { ...state.tasks[affectedElementIndex],
+                    complete: action.value
+                }
                 return newState;
             case SHOW_TASKS:
-                newState = { ... state, showComplete: action.value };
+                newState = { ...state,
+                    showComplete: action.value
+                };
                 return newState;
         }
         return state;
     }
-    getState(){
+    getState() {
         return this.__state;
     }
 }
@@ -87,30 +150,36 @@ const tasksStore = new TasksStore(tasksDispatcher);
 
 const render = () => {
 
-    const TaskComponent = ({content,complete,id})=>(
+    const TaskComponent = ({
+        content,
+        complete,
+        id
+    }) => (
         `<section>
-        ${content} <input type="checkbox" name="taskCompleteCheck" data-taskid=${id} ${complete ? "checked" : ""}> 
+            <a href=${link}>
+        ${content} <input type="checkbox" name="taskCompleteCheck" data-taskid=${id} ${complete ? "checked" : ""}>
+            </a>
     </section>`
     )
 
     const tasksSection = document.getElementById(`tasks`);
     const state = tasksStore.getState();
     const rendered = tasksStore.getState().tasks
-        .filter(task=>state.showComplete ? true : !task.complete)
+        .filter(task => state.showComplete ? true : !task.complete)
         .map(TaskComponent).join("");
     tasksSection.innerHTML = rendered;
 
     /* Add listeners to newly generated checkboxes */
-    document.getElementsByName('taskCompleteCheck').forEach(element=>{
-        element.addEventListener('change',(e)=>{
+    document.getElementsByName('taskCompleteCheck').forEach(element => {
+        element.addEventListener('change', (e) => {
             const id = e.target.attributes['data-taskid'].value;
-            const checked= e.target.checked;
-            tasksDispatcher.dispatch(completeTaskAction(id,checked));
+            const checked = e.target.checked;
+            tasksDispatcher.dispatch(completeTaskAction(id, checked));
         })
     });
 };
 
-document.forms.newTask.addEventListener('submit',(e)=>{
+document.forms.newTask.addEventListener('submit', (e) => {
     e.preventDefault();
     const name = e.target.newTaskName.value;
     if (name) {
@@ -119,17 +188,19 @@ document.forms.newTask.addEventListener('submit',(e)=>{
     }
 });
 
-document.forms.undo.addEventListener('submit',(e)=>{
+document.forms.undo.addEventListener('submit', (e) => {
     e.preventDefault();
     tasksStore.revertLastState();
 })
 
-document.getElementById(`showComplete`).addEventListener('change',({target})=>{
+document.getElementById(`showComplete`).addEventListener('change', ({
+    target
+}) => {
     const showComplete = target.checked;
     tasksDispatcher.dispatch(showTasksAction(showComplete));
 });
 
-tasksStore.addListener(()=>{
+tasksStore.addListener(() => {
     render();
 });
 
